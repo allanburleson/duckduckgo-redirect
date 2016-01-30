a283acc15dda65a7c8e6628c794f32ef34c03ad5
allan <disorientedperson>
2016-01-30 14:35:47 -0600

    Remove console logging because Mozilla doesn't allow it

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
-var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
-
-var button = buttons.ActionButton({
-  id: "mozilla-link",
-  label: "Visit Mozilla",
-  icon: {
-    "16": "./icon-16.png",
-    "32": "./icon-32.png",
-    "64": "./icon-64.png"
-  },
-  onClick: handleClick
-});
-
-function handleClick(state) {
-  tabs.open("https://duckduckgo.com");
-}
 
 tabs.on('ready', function(tab) {
   var taburl = tab.url
   if ((taburl.startsWith('www.google.com') || taburl.startsWith('https://www.google.com')) || taburl.startsWith('http://www.google.com')) {
     if (taburl.indexOf('/search?') != -1) {
       var search = taburl.slice(taburl.indexOf('?q'));
-      console.log('Redirecting...')
       tab.url = 'https://duckduckgo.com/' + search;
     }
-    else {
-      console.log('Not going to redirect.');
-    }
-    console.log(taburl);
-    
   }
 });
