aaa8f780ec26dd5b7b2c4b46c20d36d124285eb8
disorientedperson <disorientedperson@icloud.com>
2016-01-31 12:51:12 -0600

    Redirect without loading original page

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-var tabs = require("sdk/tabs");
+const self = require("sdk/self");
+const tabs = require("sdk/tabs");
+const events = require("sdk/system/events");
+const chrome = require("chrome");
 
-tabs.on('ready', function(tab) {
-  var taburl = tab.url
-  var urlre = /^https*:\/\/www\.google.com/
-  if (urlre.test(taburl)) {//((taburl.startsWith('www.google.com') || taburl.startsWith('https://www.google.com')) || taburl.startsWith('http://www.google.com')) {
-    if (taburl.indexOf('/search?') != -1) {
-      var search = taburl.slice(taburl.indexOf('.com/') + 4);
-      if (/\?q=[a-z\+%0-9]*/.test(search)) {
-        search = search.match(/\?q=[a-z\+%0-9]*/)[0];
-        tab.url = 'https://duckduckgo.com/' + search;
-      }
-      else if (/&q=[a-z\+%0-9]*/.test(search)) {
-        search = '?' + search.match(/&q=[a-z\+%0-9]*/)[0].slice(1); // Get search info, remove &, add ?
-        tab.url = 'https://duckduckgo.com/' + search;
+function listener(event) {
+  var channel = event.subject.QueryInterface(chrome.Ci.nsIHttpChannel);
+  var replace = false;
+  var firstURL = event.subject.URI.spec;
+  if(firstURL.match(/^https*:\/\/www\.google.com/)) {
+    var ioService = chrome.Cc["@mozilla.org/network/io-service;1"].getService(chrome.Ci.nsIIOService);
+    var newURL = 'https://duckduckgo.com';
+    if (firstURL.indexOf('/search?') != -1) {
+      var searchre = /[\?#&]q=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
+      var search = firstURL.slice(firstURL.indexOf('.com/') + 4);
+      if (searchre.test(search)) {
+        search = '?' + search.match(searchre)[0].slice(1);
       }
     }
+    else {
+      var search = '';
+    }
+    newURL += search;
+    var newURI = ioService.newURI(newURL, event.subject.URI.originCharset, null);
+    channel.redirectTo(newURI);
   }
-});
+  return;
+}
+
+events.on("http-on-modify-request", listener);
