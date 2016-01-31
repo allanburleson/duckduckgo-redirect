11f1670efc5734cd6a1e25cdd564487af2c2cbd9
disorientedperson <disorientedperson@icloud.com>
2016-01-31 14:14:12 -0600

    Final change before 1.3 release

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const self = require("sdk/self");
-const tabs = require("sdk/tabs");
-const events = require("sdk/system/events");
-const chrome = require("chrome");
+var self = require("sdk/self");
+var tabs = require("sdk/tabs");
+var events = require("sdk/system/events");
+var chrome = require("chrome");
 
 function listener(event) {
   var channel = event.subject.QueryInterface(chrome.Ci.nsIHttpChannel);
   var replace = false;
   var firstURL = event.subject.URI.spec;
-  if(firstURL.match(/^https*:\/\/www\.google\.com/)) {
+  // Find what search engine is being used (if any) and change regex
+  if(firstURL.match(/^https*:\/\/www\.google\.com/)) { 
     var searchre = /[\?#&]q=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
   }
   else if (firstURL.match(/^https*:\/\/www\.bing\.com/)) {
@@ -20,16 +21,20 @@ function listener(event) {
     searchre = null;
   }
   if (searchre != null) {
+    // If the variable contains a regex
     var ioService = chrome.Cc["@mozilla.org/network/io-service;1"].getService(chrome.Ci.nsIIOService);
     var newURL = 'https://duckduckgo.com';
     if (firstURL.indexOf('/search?') != -1) {
       
       var search = firstURL.slice(firstURL.indexOf('.com/') + 4);
       if (searchre.test(search)) {
+        // remove ?q or &q or #q from match and add ?q to ensure URL
+        // ... is setup the right way
         search = '?q' + search.match(searchre)[0].slice(2);
       }
     }
     else {
+      // don't add anything to the duckduckgo.com URL
       var search = '';
     }
     newURL += search;
