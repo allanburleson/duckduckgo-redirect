00043d06e7d924d0dd2202d295fa9d86c478a334
disorientedperson <disorientedperson@icloud.com>
2016-01-31 13:25:50 -0600

    Multiple search engine support!

--- index.js
+++ index.js
@@ -7,14 +7,26 @@ function listener(event) {
   var channel = event.subject.QueryInterface(chrome.Ci.nsIHttpChannel);
   var replace = false;
   var firstURL = event.subject.URI.spec;
-  if(firstURL.match(/^https*:\/\/www\.google.com/)) {
+  if(firstURL.match(/^https*:\/\/www\.google\.com/)) {
+    var searchre = /[\?#&]q=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
+  }
+  else if (firstURL.match(/^https*:\/\/www\.bing\.com/)) {
+    var searchre = /[\?#&]q=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
+  }
+  else if (firstURL.match(/^https*:\/\/search\.yahoo\.com/)) {
+    var searchre = /[\?#&]p=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
+  }
+  else {
+    searchre = null;
+  }
+  if (searchre != null) {
     var ioService = chrome.Cc["@mozilla.org/network/io-service;1"].getService(chrome.Ci.nsIIOService);
     var newURL = 'https://duckduckgo.com';
     if (firstURL.indexOf('/search?') != -1) {
-      var searchre = /[\?#&]q=[a-z\+%0-9@\/-;\.<>,\(\)]*/i;
+      
       var search = firstURL.slice(firstURL.indexOf('.com/') + 4);
       if (searchre.test(search)) {
-        search = '?' + search.match(searchre)[0].slice(1);
+        search = '?q' + search.match(searchre)[0].slice(2);
       }
     }
     else {
