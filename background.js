851f6f24d1894f178bdb2336be04d3585043f455
Allan Burleson <allanburleson@users.noreply.github.com>
2018-06-08 14:57:09 -0500

    Add Bing and Yahoo

--- background.js
+++ background.js
@@ -1,14 +1,20 @@
 var filter = {
   url:
   [
-    {hostContains: "google.com"}
+    {hostContains: 'google.com'},
+    {hostContains: 'bing.com'},
+    {hostContains: 'yahoo.com'}
   ]
 };
 
 function get_query(url) {
   // Finds search query in url
   // 1st capture group contains only query itself
-  re = /[\?#&]q=([^&?#]+)/i;
+  if (url.includes('search.yahoo.com')) {
+    var re = /[\?#&]p=([^&?#]+)/i;
+  } else {
+    var re = /[\?#&]q=([^&?#]+)/i;
+  }
   var match = url.match(re);
   if (match) {
     return match[1];
@@ -22,6 +28,8 @@ function redirect(details) {
   var query = get_query(url);
   var newurl = 'https://duckduckgo.com/?q=' + query;
   console.log(url + ' => ' + newurl);
+  // Changes URL to newurl
+  // Current problems with opening search in new tab
   var updating = browser.tabs.update({url: newurl});
   
 };
